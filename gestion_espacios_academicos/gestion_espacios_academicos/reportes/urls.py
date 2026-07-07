a83e94fad850c6e1ccd9abd262e337e6f503868f
carmenmirna-is <carmenmirna93@gmail.com>
2026-07-07 11:39:11 -0400

    limpieza

--- gestion_espacios_academicos/gestion_espacios_academicos/reportes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'reportes'
-
-urlpatterns = [
-    path('', views.generar_reportes, name='generar_reportes'),  # Ruta para generar reportes
-    # Puedes agregar más rutas relacionadas con reportes aquí
-]
